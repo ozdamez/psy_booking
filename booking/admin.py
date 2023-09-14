d35be70ad9835b5bbd4a82779a43047fa2fcfd85
ozdamez <ozdamez@gmail.com>
2023-09-14 21:27:09 +0200

    Dopisane uslugi, godziny przyjeć. Utworzona autoryzacja.

--- booking/admin.py
+++ booking/admin.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
-from .models import Person, Visit, Place, Booking
+from .models import Appointment
 # Register your models here.
-admin.site.register(Person)
-admin.site.register(Place)
-admin.site.register(Visit)
-admin.site.register(Booking)
+
+admin.site.register(Appointment)
--- booking/models.py
+++ booking/models.py
@@ -1,31 +1,36 @@
 from django.db import models
+from datetime import datetime
+from django.contrib.auth.models import User
 
+SERVICE_CHOICES = (
+    ("Terapia indywidualna", "Terapia indywidualna"),
+    ("Terapia par", "Terapia par"),
+    ("Terapia rodziny", "Terapia rodziny"),
+    ("Coaching kariery", "Coaching kariery"),
+    )
+TIME_CHOICES = (
+    ("08:00", "08:00"),
+    ("09:00", "09:00"),
+    ("10:00", "10:00"),
+    ("11:00", "11:00"),
+    ("12:00", "12:00"),
+    ("13:00", "13:00"),
+    ("14:00", "14:00"),
+    ("15:00", "15:00"),
+    ("16:00", "16:00"),
+    ("17:00", "17:00"),
+    ("18:00", "18:00"),
+    ("19:00", "19:00"),
+    ("20:00", "20:00"),
+    ("21:00", "21:00"),
 
-class Person(models.Model):
-    name = models.CharField(max_length=200)
-    email = models.EmailField(max_length=200)
-    tel_num = models.CharField(max_length=9)
+)
 
+class Appointment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    service = models.CharField(max_length=50, choices=SERVICE_CHOICES, default="Terapia indywidualna")
+    day = models.DateField(default=datetime.now)
+    time = models.CharField(max_length=10, choices=TIME_CHOICES, default="14:00")
+    time_ordered = models.DateTimeField(default=datetime.now, blank=True)
     def __str__(self):
-        return self.name
-
-class Place(models.Model):
-    name = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.name
-class Visit(models.Model):
-    name = models.CharField(max_length=200)
-    price = models.IntegerField
-
-    def __str__(self):
-        return self.name
-
-class Booking(models.Model):
-    person = models.ForeignKey(Person, on_delete=models.CASCADE)
-    place = models.ForeignKey(Place, on_delete=models.CASCADE)
-    visit = models.ForeignKey(Visit, on_delete=models.CASCADE)
-    dt = models.DateTimeField()
-
-    def __str__(self):
-        return self.person.name + ' : ' + self.place.name + ' : ' + str(self.dt) + ' : ' + self.visit.name
+        return f"{self.user.username} | day: {self.day} | time: {self.time}"
